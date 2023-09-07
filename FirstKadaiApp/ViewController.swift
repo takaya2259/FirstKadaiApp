ba682c3225db789f88c7e994a1ad309586f02234
takaya <tky.ishimori@gmail.com>
2023-09-07 23:15:06 +0900

    はじめてのアプリ課題作成完了

--- FirstKadaiApp/ViewController.swift
+++ FirstKadaiApp/ViewController.swift
@@ -7,34 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-    @IBOutlet weak var tableView: UITableView!
-    
+class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        tableView.fillerRowHeight = UITableView.automaticDimension
-        
-        tableView.dataSource = self
-        tableView.delegate = self
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        cell.textLabel!.text = "Row\(indexPath.row)"
-        
-        return cell
-        
-    }
-
 
 }
 
